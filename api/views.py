f424ee46de5d4b75b09e65c298b53506e9d57215
Niramay33 <thakerniramay@gmail.com>
2025-01-04 08:27:44 +0530

    Created model and populated date

---
+++ api/views.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_product(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    
+    serializer = ProductSerializer(product)
+    
+    return Response(serializer.data)
